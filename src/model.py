98e50fd162ef22d5c3827d4ca6a8ee189f9c93b5
Daniele Genovese <delitedaniele@gmail.com>
2025-12-17 11:41:48 +0100

    GPTModel V1

--- src/model.py
+++ src/model.py
@@ -1,11 +1,15 @@
 import torch
 import torch.nn as nn
+from dataset import TransformerEmbedding
+
 
 class MultiHeadAttention(nn.Module):
-    def __init__(self, d_in, d_out, context_length, num_heads, dropout, qkv_bias=False):
+    def __init__(
+        self, d_in, d_out, context_length, num_heads, mha_dropout=0.1, qkv_bias=False
+    ):
         super().__init__()
-        assert (d_out % num_heads) == 0, 'd_out must be divisble by num_heads'
-        
+        assert (d_out % num_heads) == 0, "d_out must be divisble by num_heads"
+
         self.num_heads = num_heads
         self.d_out = d_out
         self.head_dim = d_out // num_heads
@@ -16,11 +20,10 @@ class MultiHeadAttention(nn.Module):
 
         self.out_proj = nn.Linear(d_out, d_out)
 
-        self.dropout = nn.Dropout(dropout)
+        self.dropout = nn.Dropout(mha_dropout)
 
         self.register_buffer(
-            'mask',
-            torch.tril(torch.ones(context_length, context_length), diagonal=1)
+            "mask", torch.tril(torch.ones(context_length, context_length), diagonal=1)
         )
 
     def forward(self, x):
@@ -31,25 +34,28 @@ class MultiHeadAttention(nn.Module):
         values = self.W_value(x)
 
         keys = keys.view(b, num_tokens, self.num_heads, self.head_dim).traspose(1, 2)
-        queries = queries.view(b, num_tokens, self.num_heads, self.head_dim).transpose(1, 2)
-        values = values.view(b, num_tokens, self.num_heads, self.head_dim).transpose(1, 2)
+        queries = queries.view(b, num_tokens, self.num_heads, self.head_dim).transpose(
+            1, 2
+        )
+        values = values.view(b, num_tokens, self.num_heads, self.head_dim).transpose(
+            1, 2
+        )
 
-        scores = queries @ keys.transpose(2,3)
+        scores = queries @ keys.transpose(2, 3)
         mask_bool = self.mask.bool()[:num_tokens, :num_tokens]
         scores.masked_fill(mask_bool, -torch.inf)
 
-        attn_weights = torch.softmax(scores / keys.shape[-1]**0.5, dim=-1)
+        attn_weights = torch.softmax(scores / keys.shape[-1] ** 0.5, dim=-1)
         attn_weights = self.dropout(attn_weights)
-        context_vec = (attn_weights @ values).transpose(1,2)
+        context_vec = (attn_weights @ values).transpose(1, 2)
 
-        context_vec = context_vec.contiguous().view(
-            b, num_tokens, self.d_out
-        )
+        context_vec = context_vec.contiguous().view(b, num_tokens, self.d_out)
 
         context_vec = self.out_proj(context_vec)
 
         return context_vec
-        
+
+
 class LayerNorm(nn.Module):
     def __init__(self, emd_dim, eps=1e-5):
         super().__init__()
@@ -63,19 +69,16 @@ class LayerNorm(nn.Module):
         normalized_x = (x - mean) / torch.sqrt(variance + self.eps)
         return self.scale * normalized_x + self.shift
 
+
 class FeedForward(nn.Module):
-    def __init__(self, emb_dim, hidden_dim):
+    def __init__(self, emb_dim, hidden_dim, ff_dropout=0.1):
         super().__init__()
         self.layers = nn.Sequential
-        (
-            nn.Linear(emb_dim, hidden_dim),
-            nn.GELU(),
-            nn.Linear(hidden_dim, emb_dim)
-        )
+        (nn.Linear(emb_dim, hidden_dim), nn.GELU(), nn.Linear(hidden_dim, emb_dim))
 
     def forward(self, x):
         return self.layers(x)
-    
+
 
 class TransformerBlock(nn.Module):
     def __init__(self, emb_dim, num_heads, dropout, hidden_dim, qkv_bias=False):
@@ -86,13 +89,10 @@ class TransformerBlock(nn.Module):
             context_length=512,
             num_heads=num_heads,
             dropout=dropout,
-            qkv_bias=qkv_bias
+            qkv_bias=qkv_bias,
         )
 
-        self.ff = FeedForward(
-            emb_dim=emb_dim,
-            hidden_dim=hidden_dim
-        )
+        self.ff = FeedForward(emb_dim=emb_dim, hidden_dim=hidden_dim)
 
         self.norm1 = LayerNorm(emb_dim)
         self.norm2 = LayerNorm(emb_dim)
@@ -113,4 +113,46 @@ class TransformerBlock(nn.Module):
         x = shortcut + x
 
         return x
-    
+
+
+class GPTModel(nn.Module):
+    def __init__(
+        self,
+        vocab_size,
+        emb_dim,
+        max_length,
+        num_heads,
+        num_layers,
+        dropout,
+        qkv_bias=False,
+    ):
+        super().__init__()
+        self.tok_emb = TransformerEmbedding(
+            n_vocab=vocab_size,
+            max_length=max_length,
+            out_dim=emb_dim,
+            dropout_rate=dropout,
+        )
+
+        self.trf_blocks = nn.ModuleList(
+            *[
+                TransformerBlock(
+                    emb_dim=emb_dim,
+                    num_heads=num_heads,
+                    dropout=dropout,
+                    hidden_dim=4 * emb_dim,
+                    qkv_bias=qkv_bias,
+                )
+                for _ in range(num_layers)
+            ]
+        )
+
+        self.final_norm = LayerNorm(emb_dim)
+        self.out_head = nn.Linear(emb_dim, vocab_size, bias=False)
+
+    def forward(self, in_idx):
+        x = self.tok_emb(in_idx)
+        x = self.trf_blocks(x)
+        x = self.final_norm(x)
+        logits = self.out_head(x)
+        return logits
--- src/train.py
+++ src/train.py
@@ -1,4 +1,3 @@
-
 import requests
 from dataset import TransformerEmbedding, create_dataloader_v1
 import config
@@ -10,24 +9,14 @@ if __name__ == "__main__":
     embedder = TransformerEmbedding(
         n_vocab=config.VOCAB_SIZE,
         max_length=config.MAX_LENGTH,
-        out_dim=config.OUTPUT_DIM
+        out_dim=config.OUTPUT_DIM,
     )
 
     dataloader = create_dataloader_v1(
-        txt=text, 
-        batch_size=config.BATCH_SIZE,
-        num_workers=config.NUM_WORKERS
+        txt=text, batch_size=config.BATCH_SIZE, num_workers=config.NUM_WORKERS
     )
 
     input_ids, target_ids = next(iter(dataloader))
     embeddings = embedder(input_ids)
-
-    model = MultiHeadAttention(
-        d_in=config.EMBED_DIM,
-        d_out=config.OUTPUT_DIM,
-        context_length=config.MAX_LENGTH,
-        num_heads=2,
-        dropout=0.1
-    )
 
     print(embeddings.shape)
